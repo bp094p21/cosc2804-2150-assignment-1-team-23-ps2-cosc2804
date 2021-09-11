d7295d654c649498701cf68796dc67862f0e584b
huntermuze <mbyamen@gmail.com>
2021-09-11 15:48:33 +1000

    enforce naming convention, implement plot's build function

--- villagegenerator/core/layout/plot.py
+++ villagegenerator/core/layout/plot.py
@@ -1,9 +1,18 @@
+from villagegenerator.structure.property import property
+
+
 # contains the orientation, position and centre of where the structure(s) and road(s) should go.
 # this is passed down to the structure/road classes, so that they can position themselves accordingly.
 class Plot:
-    def __init__(self, contained):
-        self.contained = contained
+    def __init__(self, item):
+        self.item = item
 
     # called to build each house/road when iterating through the predefined layout.
     def build(self):
-        pass
+        # if its a property, invoke the build method.
+        if type(self.item) is type(property):
+            self.item.build()
+            return
+
+        # if its a road, call the function as is.
+        self.item()
---
+++ villagegenerator/structure/property/property.py
@@ -0,0 +1,7 @@
+import floor, front, house, pool, roof, room, wall
+
+class Property:
+    roadside_matrix_el = None
+
+    def __init__(self, x, y, z, matrix_pos,  ):
+        pass
