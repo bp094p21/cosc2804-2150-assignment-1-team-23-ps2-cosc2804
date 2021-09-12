c0829aaa65f4520bccc8ef69f0542727c08d0d45
Fibre P <fttx5525@gmail.com>
2021-09-12 19:57:32 +1000

    testing component building with orientation considered

--- villagegenerator/construction/building/property/tests/component_test.py
+++ villagegenerator/construction/building/property/tests/component_test.py
@@ -10,34 +10,30 @@ mc = minecraft.Minecraft.create()
 # Get player's current position
 player_pos = mc.player.getPos()
 # Set build_pos to 5 blocks in front of player
-build_pos = vec3.Vec3(player_pos.x + 5, player_pos.y, player_pos.z)
+build_pos = vec3.Vec3(player_pos.x + 5, player_pos.y-3, player_pos.z)
 
-entrance = component.Entrance()
-# Give entrance a block_list
-entrance.block_list = []
+wall = component.Component('wall')
+# Give wall a block_list
+wall.block_list = []
 for y_inc in range(3):
     for x_inc in range(15):
         for z_inc in range(1):
-            entrance.block_list.append(4)
-# Give entrance dimensions
-entrance.z_len = 1
-entrance.x_len = 15
-entrance.y_len = 3
-entrance.build(build_pos, mc)
+            wall.block_list.append(4)
+# Give wall dimensions
+wall.z_len = 1
+wall.x_len = 15
+wall.y_len = 3
+# Build four walls with 4 different orientations
+for i in range(4):
+    wall.build(build_pos, mc, i)
 
-door = component.Component('door')
-door.block_list = []
-door.z_len = 1
-door.x_len = 2
-door.y_len = 2
-for i in range(door.y_len):
-    for j in range(door.x_len):
-        for k in range(door.z_len):
-            door.block_list.append(46)
-door.build(vec3.Vec3(build_pos.x+7, build_pos.y, build_pos.z), mc)
-all_blocks = component.Component('all_blocks')
-
-all_blocks.block_list = []
-all_blocks.z_len = 1
-for i in range(256):
-    mc.setBlock(build_pos.x, build_pos.y, build_pos.z + i, i)
+# door = component.Component('door')
+# door.block_list = []
+# door.z_len = 1
+# door.x_len = 2
+# door.y_len = 2
+# for i in range(door.y_len):
+#     for j in range(door.x_len):
+#         for k in range(door.z_len):
+#             door.block_list.append(46)
+# door.build(vec3.Vec3(build_pos.x+7, build_pos.y, build_pos.z), mc, 1)
