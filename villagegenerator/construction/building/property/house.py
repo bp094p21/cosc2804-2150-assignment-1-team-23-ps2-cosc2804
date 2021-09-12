61e9363ba6fb6b6779a5430bfe495fd84e786f0e
Fibre P <fttx5525@gmail.com>
2021-09-12 15:56:49 +1000

    Init Commit

--- villagegenerator/construction/building/property/house.py
+++ villagegenerator/construction/building/property/house.py
@@ -5,16 +5,16 @@ class House:
 
 class SingleStory(House):
     def __init__(self):
-        self.type = 'Single Story House'
+        self.type = 'single_story'
 
 class DoubleStory(House):
     def __init__(self):
-        self.type = 'Double Story House'
+        self.type = 'double_story'
 
 class Unit(House):
     def __init__(self):
-        self.type = 'Unit'
+        self.type = 'unit'
 
 class Apartment(House):
     def __init__(self):
-        self.type = 'Apartment'
+        self.type = 'apartment'
