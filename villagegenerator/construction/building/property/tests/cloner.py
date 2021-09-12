d31ba4bdde35c3154911ed050412e6dce7a40c28
Fibre P <fttx5525@gmail.com>
2021-09-12 21:01:07 +1000

    move cloner to util and add main testing program on file exec

--- villagegenerator/construction/building/property/tests/cloner.py
+++
@@ -1,3 +0,0 @@
-class Cloner:
-    def __init__(self):
-        pass
---
+++ villagegenerator/construction/building/property/util/cloner.py
@@ -0,0 +1,32 @@
+from mcpi import minecraft
+from mcpi import vec3
+import sys
+sys.path.append('../property')
+import component
+
+
+class Cloner:
+    def __init__(self, mc: minecraft.Minecraft, x_len=15, y_len=15, z_len=15):
+        self.mc = mc
+        self.x_len = x_len
+        self.y_len = y_len
+        self.z_len = z_len
+        pass
+    def clone(self, original_pos, x = 0, y = 0, z = 15):
+
+        blocks = self.mc.getBlocks(original_pos, original_pos.x + self.x_len - 1, original_pos.y + self.y_len - 1, original_pos.z + self.z_len - 1)
+        clone_pos = vec3.Vec3(original_pos.x + x, original_pos.y + y, original_pos.z + z)
+        clone = component.Component('clone')
+        clone.block_list = blocks
+        clone.x_len = self.x_len
+        clone.y_len = self.y_len
+        clone.z_len = self.z_len
+        clone.build(clone_pos, self.mc, 0)
+
+# TEST
+if __name__ == "__main__":
+    mc = minecraft.Minecraft.create()
+    c = Cloner(mc)
+    player_pos = mc.player.getPos()
+    original_pos = vec3.Vec3(player_pos.x + 5, player_pos.y, player_pos.z - 8)
+    c.clone(original_pos)
