292c47717b5667171eab21f8300a93952312d2be
Fibre P <fttx5525@gmail.com>
2021-09-12 15:56:27 +1000

    Make test directory

---
+++ villagegenerator/construction/building/property/tests/vector3_test.py
@@ -0,0 +1,27 @@
+# Import minecraft
+from mcpi import minecraft
+# Create connection
+mc = minecraft.Minecraft.create()
+# Get player's current position
+player_pos = mc.player.getPos()
+# Set build_pos to 5 blocks in front of player
+build_pos = player_pos[0] + 5, player_pos[1], player_pos[2]
+y, z, x = build_pos
+# Initialize block type
+block_type = 0
+
+# Set Stone blocks in x-axis
+mc.setBlocks(y,z,x, y,z,x+2, 1)
+# Set Grass blocks in y-axis
+mc.setBlocks(y,z,x, y+2,z,x, 2)
+# Set Wood blocks in z-axis
+mc.setBlocks(y,z,x, y,z+2,x, 5)
+
+entrance_component = component.Entrance()
+
+xx = 0
+yy = 0
+zz = 0
+
+for block in entrance_component:
+    mc.setBlock()
