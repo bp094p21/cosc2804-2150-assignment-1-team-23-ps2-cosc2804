1cb619c5712e5335c97c31a7f00ce27caf987d91
Fibre P <fttx5525@gmail.com>
2021-09-12 17:06:41 +1000

    use vec3

--- villagegenerator/construction/building/property/component.py
+++ villagegenerator/construction/building/property/component.py
@@ -1,28 +1,32 @@
+from mcpi import vec3
 from mcpi import minecraft
 class Component:
     block_list = []
     x_len = None
     y_len = None
     z_len = None
-    component_pos = (None, None, None)
+    x_pos = None
+    y_pos = None
+    z_pos = None
     mc = None
     type = None
-    def __init__(self):
+    def __init__(self, type=None):
+        self.type = type
         pass
-    def build(self, component_pos, mc):
-        self.component_pos = component_pos
+    def build(self, location_vec3: vec3.Vec3, mc: minecraft.Minecraft):
+        self.x_pos, self.y_pos, self.z_pos = location_vec3
         x = 0
         y = 0
         z = 0
         i = 0
         for block in self.block_list:
-            mc.setBlock(self.component_pos[0] + x, self.component_pos[1] + y, self.component_pos[2] + z, block)
+            mc.setBlock(self.x_pos + x, self.y_pos + y, self.z_pos + z, block)
             i += 1
             z += 1
             if i % self.z_len == 0:
                 z = 0
                 x += 1
-            if i % (self.x_len ** 2) == 0:
+            if i % (self.z_len * self.x_len) == 0:
                 x = 0
                 y += 1
         pass
