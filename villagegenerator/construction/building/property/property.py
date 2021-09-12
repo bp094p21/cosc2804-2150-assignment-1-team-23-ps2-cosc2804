3a257515d14d382de4e8a9a56d20c36e3a035f74
Fibre P <fttx5525@gmail.com>
2021-09-12 17:46:30 +1000

    Fix location string

--- villagegenerator/construction/building/property/property.py
+++ villagegenerator/construction/building/property/property.py
@@ -3,9 +3,8 @@ from mcpi import minecraft
 import theme
 
 class Property:
-    # Init inputs
-    location_vec3: vec3.Vec3 = None
-    property_location_str: str = f"\nx: {location_vec3.x}\ny: {location_vec3.y}\nz: {location_vec3.z}"
+    # __init__ inputs
+    location_vec3: vec3.Vec3 = None 
     entrance_edge: int = None
     biome: int = None
     mc: minecraft.Minecraft = None
@@ -21,27 +20,30 @@ class Property:
         'pool': None,
         'roof': None
     }
+    # Print message utilities
+    property_location_str: str = "Location not set"
     build_status: str = "Property not yet instantiated"
 
     # Public methods
     def __init__(self, location_vec3: vec3.Vec3, entrance_edge: int, biome: int, mc: minecraft.Minecraft):
         self.location_vec3 = location_vec3
-        self.property_location_str: str = f"\nx: {location_vec3.x}\ny: {location_vec3.y}\nz: {location_vec3.z}"
+        self.property_location_str: str = f"\nx: {location_vec3.x}\ny: {location_vec3.y}\nz: {location_vec3.z}\n"
         self.entrance_edge = entrance_edge
         self.biome = biome
         self.mc = mc
-        self.build_status = f"Property object instantiated with location:\n{self.property_location_str}"
+        self.build_status = f"✅ Property object instantiated with location:\n{self.property_location_str}"
         self.print_status()
         pass
     def print_status(self):
         print(self.build_status)
     def build(self):
-        self.build_status = f"Commencing property build at location:{self.property_location_str}"
+        self.build_status = f"🚧 Commencing property build at location:\n{self.property_location_str}"
         self.print_status()
         self._set_theme()
         self._random_select_house_type()
         self._instantiate_components()
         self._build_components()
+        self.build_status = f"✅ Completed property build at location:\n{self.property_location_str}"
         self.print_status()
     # Internal methods
     def _set_theme(self):
@@ -52,11 +54,11 @@ class Property:
             self.theme = theme.ThemeB()
         elif self.biome:
             self.theme = theme.ThemeC()
-        self.build_status = f"Theme set: {self.theme}"
+        self.build_status = f"✅ Theme set: {self.theme}\n"
         self.print_status()
     def _random_select_house_type(self):
         # TODO: Use theme to randomly select property type from a set of property types applicable to that theme
-        self.build_status = f"Property type randomly selected: {self.house_type}"
+        self.build_status = f"✅ Property type randomly selected: {self.house_type}"
         pass
     def _instantiate_components(self):
         # TODO: Use house_type to randomly instantiate components to build
