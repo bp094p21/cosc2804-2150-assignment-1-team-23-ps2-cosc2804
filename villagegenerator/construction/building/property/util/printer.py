5ea83b97301b451d7c03e2f15b9b0c9b86f2867d
Fibre P <fttx5525@gmail.com>
2021-09-12 19:55:31 +1000

    add utility class Printer

---
+++ villagegenerator/construction/building/property/util/printer.py
@@ -0,0 +1,7 @@
+class Printer:
+    def __init__(self, init_status = None):
+        self.status = init_status
+    def print_status(self):
+        print(self.status)
+    def set_location(self):
+        pass 
