58ea0e21913b11dccb8d380f29de00b16a663671
huntermuze <mbyamen@gmail.com>
2021-09-13 15:38:27 +1000

    deploy beta framework prototype

--- villagegenerator/core/layout/predefined_layouts.py
+++ villagegenerator/core/layout/predefined_layouts.py
@@ -1,100 +1,99 @@
 from plot import Plot
-from villagegenerator.core import village_generator as vg
-from villagegenerator.core.village.village_size import VillageSize
 from orientation import Orientation as O
+from villagegenerator.core.layout.layout import Layout
 
 
-# set of global variables to be accessed in the main program.
-
-# TODO - make these only be created if the user requests them. Dont all need to exist. Wrap in some sort of function.
 # TODO - all the buildings must both implement the build() method, and they must have a setter to set location.
 #  Enforce this later.
 
 
-def define_layouts(biome, mc_instance, size):
-    pass
-
-
-biome = vg.ply_biome
-mc = vg.mc
-
 # Small variations.
 # Dimensions - 8 * 4 = 32 Elements
 # Max Structures - 4
-SMALL_VARIATION_ONE = [[],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [Plot(Property(O.RIGHT, biome, mc))]]
+def define_small(mc, biome, size):
+    # Layout #1
+    Layout(size,
+           [[],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [Plot(Property(O.RIGHT, biome, mc))]])
 
-SMALL_VARIATION_TWO = [[],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       []]
+    # Layout #2
+    Layout(size,
+           [[],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []])
+
 
 # Medium variations.
 # Dimensions - 10 * 5 = 50 Elements
 # Max Structures - 6
-MED_VARIATION_ONE = [[],
-                     [],
-                     [],
-                     [],
-                     [],
-                     [],
-                     [],
-                     [],
-                     [],
-                     []]
+def define_medium(mc, biome, size):
+    # Layout #1
+    Layout(size,
+           [[],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []])
 
-MED_VARIATION_TWO = [[],
-                     [],
-                     [],
-                     [],
-                     [],
-                     [],
-                     [],
-                     [],
-                     [],
-                     []]
+    # Layout #2
+    Layout(size,
+           [[],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []])
+
 
 # Large variations.
 # Dimensions - 12 * 6 = 72 Elements
 # Max Structures - 8
-LARGE_VARIATION_ONE = [[],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       []]
-
-LARGE_VARIATION_TWO = [[],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       [],
-                       []]
-
-size_to_layout = {
-    VillageSize.SMALL: (SMALL_VARIATION_ONE, SMALL_VARIATION_TWO),
-    VillageSize.MEDIUM: (MED_VARIATION_ONE, MED_VARIATION_TWO),
-    VillageSize.LARGE: (LARGE_VARIATION_ONE, LARGE_VARIATION_TWO)
-}
+def define_large(mc, biome, size):
+    # Layout #1
+    Layout(size,
+           [[],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []])
+    # Layout #2
+    Layout(size,
+           [[],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []])
