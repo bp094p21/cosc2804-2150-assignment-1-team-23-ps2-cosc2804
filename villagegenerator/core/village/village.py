6bce08300630cbdf5051ab7c7cc71eb8ca147746
huntermuze <mbyamen@gmail.com>
2021-09-13 15:37:42 +1000

    remove village module - infeasible idea

--- villagegenerator/core/village/village.py
+++
@@ -1,6 +0,0 @@
-# consider making each village an object, so that the user can perform in-place modifications (hotswap)
-# on an existing one? such as randomising a particular village, or changing the material/biome,
-# OR potentially increasing its size (challenging though and unlikely to do).
-
-class Village:
-    pass
