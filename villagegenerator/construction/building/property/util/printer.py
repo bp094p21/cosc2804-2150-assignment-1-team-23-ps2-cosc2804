f8342456aed989488b85bdf181f4fbc57b223873
Jin <bp094p21@gmail.com>
2021-09-16 00:03:04 +1000

    deleted

--- villagegenerator/construction/building/property/util/printer.py
+++
@@ -1,8 +0,0 @@
-class Printer:
-    location_str = 'Location not set yet'
-    def __init__(self, init_status = None):
-        self.status = init_status
-    def print_status(self):
-        print(self.status)
-    def set_location(self):
-        pass 
