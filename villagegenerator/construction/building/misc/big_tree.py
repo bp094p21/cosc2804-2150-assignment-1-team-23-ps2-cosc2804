85ba561e7c8b557c60611d5a29ea42b2dd7bea3d
mrblup <s389597@student.rmit.edu.au>
2021-09-15 11:17:01 +1000

    basic misc changes

--- villagegenerator/construction/building/misc/big_tree.py
+++ villagegenerator/construction/building/misc/big_tree.py
@@ -1,21 +1,28 @@
-from park import Park
+from misc import Misc
 import random
-class bigTree(Park):
+class bigTree(Misc):
     def __init__(self) -> None:
         super().__init__()
-        self.height = random.randint(2,50)
+        self.height = random.randint(2,25)
+        if self.biome == 2:
+            self.wood = self.building_wood
+        else:
+            self.wood = self.plant_array[0]
     def big_tree_build(self,x,y,z):
         self.build_foundation(x,y,z)
-        self.mc.setBlocks(x+3,y+self.height,z+3,x+11,y+self.height+5,z+11,self.leaves)
-        self.mc.setBlocks(x+6,y,z+6,x+8,y+self.height+3,z+8,self.tree)
+        self.mc.setBlocks(x+3,y+self.height,z+3,x+11,y+self.height+5,z+11,self.plant_array[1])
+        self.mc.setBlocks(x+6,y,z+6,x+8,y+self.height+3,z+8,self.wood)
     def big_branches(self,x,y,z):
         for i in range(3):
-            self.mc.setBlocks(x+6+i,y + self.height/2 ,z+2+i ,x+8-i, y+self.height/2-i,z+5,self.leaves)
-            self.mc.setBlocks(x+6+i,y + self.height/3 ,z+10-i ,x+8, y+self.height/3-i,z+12-i,self.leaves)
-            self.mc.setBlocks(x+8,y + self.height/4 ,z+7+i ,x+12-i, y+self.height/4-i,z+9-i,self.leaves)
-            self.mc.setBlocks(x+3+i,y + self.height/1.5 ,z+6+i ,x+5, y+self.height/1.5-i,z+9-i,self.leaves) #make leaves go down y axis and shrink by oneblock
-        
+            self.mc.setBlocks(x+6+i,y + self.height/2 ,z+2+i ,x+8-i, y+self.height/2-i,z+5,self.plant_array[1])
+            self.mc.setBlocks(x+6+i,y + self.height/3 ,z+10-i ,x+8, y+self.height/3-i,z+12-i,self.plant_array[1])
+            self.mc.setBlocks(x+8,y + self.height/4 ,z+7+i ,x+12-i, y+self.height/4-i,z+9-i,self.plant_array[1])
+            self.mc.setBlocks(x+3+i,y + self.height/1.5 ,z+6+i ,x+5, y+self.height/1.5-i,z+9-i,self.plant_array[1]) #make leaves go down y axis and shrink by oneblock
+    def build(self, x,y,z):
+            self.big_tree_build(x, y, z)
+            self.big_branches(x,y,z)
+            self.build_tree(x,y,z)
+
 tree = bigTree()
-x, y, z = tree.player_position()
-tree.big_tree_build(x, y, z)
-tree.big_branches(x,y,z)
+x, y, z = tree._player_position()
+tree.build(x,y,z)
--- villagegenerator/construction/building/misc/misc.py
+++ villagegenerator/construction/building/misc/misc.py
@@ -2,55 +2,69 @@
 from mcpi.minecraft import Minecraft
 import random
 import sys
-sys.path.append('../../building')
-import property.block as b
+
+import block as b
 
 #types of misc objects{open nature area, park features, statue-like objects}
 #theme order{0= modern, 1= mediterranean, 2= magic}
-mc = Minecraft.create()
+
 
 class BuildingBlocks():
-    blocklist = {
-        'ground':[b.GRASS,b.SAND, b.MOSSY_COBBLESTONE],
-        "ground_base":[b.STONE,b.SANDSTONE,b.COBBLESTONE],
-        'main_block':[b.CONCRETE, b.TERRACOTTA,b.PURPUR_BLOCK],
-        'sub_block':[b.PILLAR_QUARTZ,b.TERRACOTTA_WHITE_GLAZED,b.OBSIDIAN],
-        'main_slab':[b.STONE_SLAB,b.STONE_SLAB.withData(1),b.PURPUR_SLAB],
-        "wood":[b.WOOD_PLANKS,b.WOOD_PLANKS.withData(4),b.WOOD_PLANKS.withData(5)],
-        'plant_stem':[b.TIMBER_LOG,b.CACTUS, b(162.1)],
-        'plant_leaves':[b.LEAVES,b.LEAVES2,b.LEAVES2.withData(1)],
-        'main_light_block':[b.SEA_LANTERN,b.SEA_LANTERN,b.GLOWSTONE],
-        'torch':[b.TORCH,b.TORCH,b.TORCH_REDSTONE],
-        'flower':[b.FLOWER_RED,b.AIR, b.MUSHROOM_RED],
-        'shrubs':[b.GRASS_TALL.withData(1),b.GRASS_TALL,b.GRASS_TALL.withData(2)],
-    }
-class Misc():
+    def __init__(self) -> None:
+        
+        self.blocklist = {
+            'ground':[b.GRASS,b.SAND, b.MOSSY_COBBLESTONE],
+            "ground_base":[b.STONE,b.SANDSTONE,b.COBBLESTONE],
+            'main_block':[b.CONCRETE, b.TERRACOTTA,b.PURPUR_BLOCK],
+            'sub_block':[b.PILLAR_QUARTZ,b.TERRACOTTA_WHITE_GLAZED,b.OBSIDIAN],
+            'main_slab':[(43,0),(43,1),(204)],
+            "wood":[b.WOOD_PLANKS,b.WOOD_PLANKS.withData(4),b.WOOD_PLANKS.withData(5)],
+            'plant_stem':[b.TIMBER_LOG,81, 162],
+            'plant_leaves':[b.LEAVES,0,b.LEAVES2.withData(1)],
+            'main_light_block':[b.SEA_LANTERN,b.SEA_LANTERN,b.GLOWSTONE],
+            'torch':[b.TORCH,b.TORCH,b.TORCH_REDSTONE],
+            'flower':[b.FLOWER_RED,b.AIR, b.MUSHROOM_RED],
+            'shrubs':[b.GRASS_TALL,31,b.GRASS_TALL.withData(2)]
+        }
+class Misc():# find  imports random misc then misc will casll global function and build that
+
     types = ["open_nature","park","statue"]
     misc_obj = None
     def __init__(self) -> None:
-        biome = None
+        self.mc = Minecraft.create()
+        self.water = 8
         block_type = None
-        if biome == 'modern':
+        self.biome = random.randint(1,3)
+        
+        """if biome == 'modern':
             block_type = 0
-        elif biome == 'mediterranean':
+        elif biome == 'sand':
+            block_type = 1
+        else:
+            block_type = 2"""
+        if self.biome == 1:
+            block_type = 0
+        elif self.biome == 2:
             block_type = 1
         else:
             block_type = 2
-        blocklist = BuildingBlocks()
-        self.air = 1
-        self.ground_array = [blocklist['ground'][block_type], blocklist['ground_base'][block_type] ]
-        self.building_array = [blocklist['main_block'][block_type],blocklist['sub_block'][block_type],blocklist['wood'][block_type]]
-        self.slab = blocklist['main_slab'][block_type]
-        self.plant_array =[blocklist['plant_stem'][block_type],blocklist['plant_leaves'][block_type]]
-        self.lighting_options = [blocklist['main_light_block'][block_type],blocklist['torch'][block_type]]
-        self.greenery = [blocklist['flower'][block_type],blocklist['shrubs'][block_type]]
+        blockthing = BuildingBlocks()
 
+        self.air = 0
+        self.ground_array = [blockthing.blocklist['ground'][block_type], blockthing.blocklist['ground_base'][block_type] ]
+        self.building_array = [blockthing.blocklist['main_block'][block_type],blockthing.blocklist['sub_block'][block_type],blockthing.blocklist['wood'][block_type]]
+        self.ground_base = blockthing.blocklist['ground_base'][block_type]
+        self.slab = blockthing.blocklist['main_slab'][block_type]
+        self.plant_array =[blockthing.blocklist['plant_stem'][block_type],blockthing.blocklist['plant_leaves'][block_type]]
+        self.lighting_options = [blockthing.blocklist['main_light_block'][block_type],blockthing.blocklist['torch'][block_type]]
+        self.greenery = [blockthing.blocklist['flower'][block_type],blockthing.blocklist['shrubs'][block_type]]
+        self.building_wood = blockthing.blocklist['wood'][block_type]
     def _player_position(self):
-        x, y, z = mc.player.getPos()
+        x, y, z = self.mc.player.getPos()
         return x, y, z
     
-    def _get_biome(self):
-        biome = mc.getBiome()
+    #def _get_biome(self):
+    #    biome = mc.getBiome()
 
     '''def _select_misc_type(self):
         i = random.randint(0,2)
@@ -65,16 +79,17 @@ class Misc():
     def place_flowers(self, x, y, z):
         for i in range(15):
             placement_x, placement_z = self.random_placement(0, 14)
-            mc.setBlocks(x + placement_x, y, z + placement_z, x + placement_x, y, z + placement_z, self.greenery[0])
+            self.mc.setBlocks(x + placement_x, y, z + placement_z, x + placement_x, y, z + placement_z, self.greenery[0])
     
     def place_grass(self, x, y, z):
         for i in range(15):
             placement_x, placement_z = self.random_placement(0, 14)
-            mc.setBlocks(x + placement_x, y, z + placement_z, x + placement_x, y, z + placement_z, self.greenery[1], 1)       
+            self.mc.setBlocks(x + placement_x, y, z + placement_z, x + placement_x, y, z + placement_z, self.greenery[1])       
 
     def build_foundation(self, x, y, z):
-        mc.setBlocks(x,y,z,x+14,y+14,z+14,self.air)
-        mc.setBlocks(x, y - 1, z, x + 14, y - 14, z + 14, self.ground_array[0])
+        self.mc.setBlocks(x,y,z,x+14,y+14,z+14,self.air)
+        self.mc.setBlocks(x, y - 1, z, x + 14, y - 14, z + 14, self.ground_array[0])
+        self.mc.setBlocks(x,y-14,z,x+14,y-14,z+14,self.ground_base)
         self.place_flowers(x, y, z)
         self.place_grass(x,y,z)
 
@@ -101,9 +116,38 @@ class Misc():
 
     def build_tree(self, x,y,z):  # builds a tree with new coordinates
         a,b,c =self.coords_tree(x,y,z)
-        mc.setBlocks(a, b, c, a, b + 5, c, self.plant_array[0])
-        mc.setBlocks(a - 2, b + 5, c - 2, a + 2, b + 7, c + 2, self.plant_array[1])
+        self.mc.setBlocks(a,b,c,a,b,c,self.ground_array[0])
+        self.mc.setBlocks(a, b+1, c, a, b + 5, c, self.plant_array[0])
+        self.mc.setBlocks(a - 2, b + 5, c - 2, a + 2, b + 7, c + 2, self.plant_array[1])
+
+"""class Park():
+    def __init__(self) -> None:  # inbiult random  function for misc will choose one of these, then this class will inherit all of the fuinction from them and thus misc will inherit the pond or soemthing, after that it will build it
+                               # perhaps create a new object /class 
+                                    # don't actually need a park class, will just import then and then misc will carry out these functions
+
+        import fountain
+        import pond
+        choice = random.randint(1,2)
+        if choice == 1:
+            park = fountain()
+        else:
+            park = pond()
+        return park
+
+class Nature():
+    def __init__(self) -> None:
+        pass
+    import big_tree
+class Statue():
+    def __init__(self) -> None:
+        pass
+    import modern_art"""
+
+
 
 park = Misc()
-park.build_foundation
-park.build_tree    
+list = BuildingBlocks()
+x,y,z = park._player_position()
+#park._get_blocks(BuildingBlocks)
+park.build_foundation(x,y,z)
+park.build_tree(x,y,z)
