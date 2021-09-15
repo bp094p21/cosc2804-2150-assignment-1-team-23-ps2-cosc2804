7d5746d2a76e72b88132cf4f77eb42da281b5a85
Jin <bp094p21@gmail.com>
2021-09-16 00:03:57 +1000

    update import statements

--- villagegenerator/construction/building/property/tradies/__init__.py
+++ villagegenerator/construction/building/property/tradies/__init__.py
@@ -1,1 +1,3 @@
+import sys
+sys.path.append('../property')
 import tradies.carpenter, tradies.carpet_call, tradies.decorator, tradies.floor_installer, tradies.gardener, tradies.jims_fencing, tradies.mason, tradies.pool_installer, tradies.roofer, tradies.tiler, tradies.tradie
