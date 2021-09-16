465a6e36b45c0f600f69e0c5a5350155d4f71cee
Lachlan <s3896004@student.rmit.edu.au>
2021-09-16 17:46:13 +1000

    roof fully completed

--- villagegenerator/construction/building/property/components/roof.py
+++ villagegenerator/construction/building/property/components/roof.py
@@ -3,9 +3,26 @@ import block as b
 from mcpi import vec3 as v
 
 class Roof(Component):
-    def __init__(self, root_v3: v.Vec3, roof_block=b.STONE_BRICK, z_len=7, x_len=5, y_len=7):
+    # Class attributes
+    type = 'roof'
+    # Instance attributes
+    root_v3: v.Vec3 = None
+    roof_block: b.Block = None
+    level: str = None
+    name: str = None
+    elevation: int = None
+    z_len: int = None
+    x_len: int = None
+    roof_v3 = {
+        'start': None,
+        'end': None
+    }
+    def __init__(self, root_v3: v.Vec3, roof_block=b.STONE_BRICK, level='ground', name='main', elevation = 5, z_len=7, x_len=5, y_len=1):
         self.root_v3 = root_v3
         self.roof_block = roof_block
+        self.level = level
+        self.name = name
+        self.elevation = elevation
         self.z_len = z_len
         self.x_len = x_len
         self.y_len = y_len
