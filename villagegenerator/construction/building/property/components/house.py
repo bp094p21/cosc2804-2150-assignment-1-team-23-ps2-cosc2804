c93e1a0a540874b60995197e3a39d7b9ca451cec
Jin <bp094p21@gmail.com>
2021-09-16 22:36:40 +1000

    add total_levels and floor_elevations attributes

--- villagegenerator/construction/building/property/components/house.py
+++ villagegenerator/construction/building/property/components/house.py
@@ -4,5 +4,7 @@ from mcpi import vec3 as v
 
 class House:
     components = {}
+    total_levels = None
+    floor_elevations = []
     def __init__(self, components={}):
         self.components = components
