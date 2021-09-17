9ef5db76b419b208053c18db1d8543b8e4dd4e68
Huntermuze <34412631+Huntermuze@users.noreply.github.com>
2021-09-17 14:17:27 +1000

    tidy up main program

--- village_generator/main.py
+++ village_generator/main.py
@@ -1,6 +1,6 @@
 from mcpi.minecraft import Minecraft
-from core import build_village
-from core import VillageSize
+from core.village.village_builder import build_village
+from core.village.village_size import VillageSize
 
 # Main class file. The VillageBuilder will be called from here to construction a village on player request (onCommand
 # event).
@@ -8,11 +8,8 @@ from core import VillageSize
 
 if __name__ == '__main__':
     mc = Minecraft.create()
-    mc.postToChat('Generating village...')
 
     x, y, z = mc.player.getTilePos()
 
     # upon invocation will generate a village where the player is standing.
-    build_village(VillageSize.SMALL, (x + 1, y, z + 1), mc.getBiome(), mc)
-
-    mc.postToChat('Done!')
+    build_village(VillageSize.SMALL, (x + 1, y, z + 1), mc.player.getBiome(), mc)
