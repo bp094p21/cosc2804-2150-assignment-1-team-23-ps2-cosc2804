0cc07818400dc38b89fd5592d80e134f4aae0ea0
Jin <bp094p21@gmail.com>
2021-09-21 14:15:58 +1000

    add attr's for different roof blocks

--- village_generator/construction/building/property/components/roof.py
+++ village_generator/construction/building/property/components/roof.py
@@ -6,15 +6,20 @@ class Roof(Component):
     # Class attributes
     type = 'roof'
     # Instance attributes
-    roof_block: b.Block = None
+    stair_block: b.Block = None
+    slab_block: b.Block = None
+    cube_block: b.Block = None
     elevation: int = None
     roof_v3 = {
         'start': None,
         'end': None
     }
-    def __init__(self, start_v3: v.Vec3, end_v3: v.Vec3, roof_block=b.STAIRS_WOOD):
+    def __init__(self, start_v3: v.Vec3, end_v3: v.Vec3, stair_block, slab_block, cube_block):
         self.roof_v3['start'] = start_v3
         self.roof_v3['end'] = end_v3
+        self.stair_block = stair_block
+        self.slab_block = slab_block
+        self.cube_block = cube_block
 
 if __name__ == '__main__':
     from mcpi import minecraft as m
