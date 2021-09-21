f3a8f42679ff43eead3236cf3f931e53c7c33590
Jin <bp094p21@gmail.com>
2021-09-21 22:39:50 +1000

    add window component import

--- village_generator/construction/building/property/components/__init__.py
+++ village_generator/construction/building/property/components/__init__.py
@@ -1,4 +1,4 @@
 import sys
 sys.path.append('../property')
 
-import components.boundary, components.carpet, components.component, components.door, components.entrance, components.floor, components.house, components.path, components.pool, components.roof, components.roof, components.room, components.stairs, components.steps, components.wall, components.flower_bed, components.tree, components.veggie_patch
+import components.boundary, components.carpet, components.component, components.door, components.entrance, components.floor, components.house, components.path, components.pool, components.roof, components.roof, components.room, components.stairs, components.steps, components.wall, components.flower_bed, components.tree, components.veggie_patch, components.window
