29ac8f4534909a0226f9a6adf168289f88ae97e4
Jin <bp094p21@gmail.com>
2021-09-21 22:40:37 +1000

    add window maker to import

--- village_generator/construction/building/property/tradies/__init__.py
+++ village_generator/construction/building/property/tradies/__init__.py
@@ -1,3 +1,3 @@
 import sys
 sys.path.append('../property')
-import tradies.carpenter, tradies.carpet_call, tradies.decorator, tradies.floor_installer, tradies.gardener, tradies.jims_fencing, tradies.landscaper, tradies.mason, tradies.pool_installer, tradies.roofer, tradies.tiler, tradies.tradie
+import tradies.carpenter, tradies.carpet_call, tradies.decorator, tradies.floor_installer, tradies.gardener, tradies.jims_fencing, tradies.landscaper, tradies.mason, tradies.pool_installer, tradies.roofer, tradies.tiler, tradies.tradie, tradies.window_maker
