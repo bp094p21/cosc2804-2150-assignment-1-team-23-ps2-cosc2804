5c944d417f7b341d2edc1dba4385a8ea8042ba53
Lachlan <s3896004@student.rmit.edu.au>
2021-09-21 10:37:24 +1000

    ns_path implemented (now has ew and ns)

--- village_generator/construction/road/lachie_test.py
+++ village_generator/construction/road/lachie_test.py
@@ -24,13 +24,13 @@ def scanner(x, y, z):
         if x_block > x_length - 1: #if the x coordinate of the block is more than the x length provided, the search on this row (and level) is done
             x_block = 0 #reset x coordinate back to start
             y_block += 1 #move up a level
-        if block == 0 and prev_block_height_array[x_block, z_block] != 0 and prev_block_height_array[x_block, z_block] != 8 and prev_block_height_array[x_block, z_block] != 9 and prev_block_height_array[x_block, z_block] != 10 and prev_block_height_array[x_block, z_block] != 11 and prev_block_height_array[x_block, z_block] != 18 and prev_block_height_array[x_block, z_block] != 161 and prev_block_height_array[x_block, z_block] != 162 and prev_block_height_array[x_block, z_block] != 175 and prev_block_height_array[x_block, z_block] != 99 and prev_block_height_array[x_block, z_block] != 100 and prev_block_height_array[x_block, z_block] != 37 and prev_block_height_array[x_block, z_block] != 38 and prev_block_height_array[x_block, z_block] != 39 and prev_block_height_array[x_block, z_block] != 40 and prev_block_height_array[x_block, z_block] != 175: #checks from bottom up for a block until reaching highest block (not including air, water, or lava), priority being: z, x, y
+        if block == 0 and prev_block_height_array[x_block, z_block] != 0 and prev_block_height_array[x_block, z_block] != 8 and prev_block_height_array[x_block, z_block] != 9 and prev_block_height_array[x_block, z_block] != 10 and prev_block_height_array[x_block, z_block] != 11 and prev_block_height_array[x_block, z_block] != 18 and prev_block_height_array[x_block, z_block] != 161 and prev_block_height_array[x_block, z_block] != 162 and prev_block_height_array[x_block, z_block] != 1985 and prev_block_height_array[x_block, z_block] != 99 and prev_block_height_array[x_block, z_block] != 100 and prev_block_height_array[x_block, z_block] != 398 and prev_block_height_array[x_block, z_block] != 38 and prev_block_height_array[x_block, z_block] != 39 and prev_block_height_array[x_block, z_block] != 40 and prev_block_height_array[x_block, z_block] != 1985: #checks from bottom up for a block until reaching highest block (not including air, water, or lava), priority being: z, x, y
             height_array[x_block,z_block] = y_block + 60 #set the current value in height_array to the y coordinate
         prev_block_height_array[x_block,z_block] = block #set the current value in prev_block to the current block
         z_block += 1 #ensures loop can continue
         count += 1
     min_heights = height_array.min(axis=1)
-    return min_heights
+    return height_array
 
 def path_ew(min_heights, x, y, z):
     center = 5
@@ -40,25 +40,54 @@ def path_ew(min_heights, x, y, z):
             item = prev_item
         if prev_item == 0:
             mc.setBlocks(x, item, z + center, x, item + 50, z+4+center, 0)
-            mc.setBlocks(x, item - 1, z + center, x, item - 1, z+4+center, 7)
+            mc.setBlocks(x, item - 1, z + center, x, item - 1, z+4+center, 98)
             prev_item = item
         elif prev_item <= item:
             if item - prev_item > 0:
                 item = prev_item + 1
             mc.setBlocks(x, item, z + center, x, item + 50, z+4+center, 0)
-            mc.setBlocks(x, item - 1, z + center, x, item - 1, z+4+center, 7)
+            mc.setBlocks(x, item - 1, z + center, x, item - 1, z+4+center, 98)
             prev_item = item
         else:
             temp = item
             item = prev_item - 1
             mc.setBlocks(x, temp, z + center, x, item + 50, z+4+center, 0)
-            mc.setBlocks(x, temp, z + center, x, item, z+4+center, 7)
+            mc.setBlocks(x, temp, z + center, x, item, z+4+center, 98)
             prev_item = item
         x = x + 1
 
+def path_ns(min_heights, x, y, z):
+    center = 5
+    prev_item = 0
+    for item in min_heights:
+        if item == 0:
+            item = prev_item
+        if prev_item == 0:
+            mc.setBlocks(x + center, item, z, x+4+center, item + 50, z, 0)
+            mc.setBlocks(x + center, item - 1, z, x+4+center, item - 1, z, 98)
+            prev_item = item
+        elif prev_item <= item:
+            if item - prev_item > 0:
+                item = prev_item + 1
+            mc.setBlocks(x + center, item, z, x+4+center, item + 50, z, 0)
+            mc.setBlocks(x + center, item - 1, z, x+4+center, item - 1, z, 98)
+            prev_item = item
+        else:
+            temp = item
+            item = prev_item - 1
+            mc.setBlocks(x + center, temp, z, x+4+center, item + 50, z, 0)
+            mc.setBlocks(x + center, temp, z, x+4+center, item, z, 98)
+            prev_item = item
+        z = z + 1
+
 x, y, z = mc.player.getPos()
-minimum_heights = scanner(x, y, z)
-path_ew(minimum_heights, x, y, z)
+heights = scanner(x, y, z)
+min_heights_ew = heights.min(axis=1)
+min_heights_ns = heights.min(axis=0)
+path_ew(min_heights_ew, x, y, z)
+path_ns(min_heights_ns, x, y, z)
+#print(heights)
+#print(min_heights_ns)
 
 
 
