291ed1c17db24ce838d542e3b7ee9e3aaeed00a1
Lachlan <s3896004@student.rmit.edu.au>
2021-09-21 22:30:45 +1000

    house checker created

--- village_generator/construction/road/lachie_test.py
+++ village_generator/construction/road/lachie_test.py
@@ -4,7 +4,7 @@ import math
 mc = minecraft.Minecraft.create()
 
 def scanner(x, y, z):
-    z_length = 180 #z_length by x_length dimensions (feel free to edit how you please to test different areas, this will later just call upon the village size)
+    z_length = 15 #z_length by x_length dimensions (feel free to edit how you please to test different areas, this will later just call upon the village size)
     x_length = 15
     mc.setBlocks(x, y, z, x, y, z+3, 0)
     acceptable_height = y
@@ -25,12 +25,11 @@ def scanner(x, y, z):
         if x_block > x_length - 1: #if the x coordinate of the block is more than the x length provided, the search on this row (and level) is done
             x_block = 0 #reset x coordinate back to start
             y_block += 1 #move up a level
-        if block == 0 and prev_block_height_array[x_block, z_block] != 0 and prev_block_height_array[x_block, z_block] != 8 and prev_block_height_array[x_block, z_block] != 9 and prev_block_height_array[x_block, z_block] != 10 and prev_block_height_array[x_block, z_block] != 11 and prev_block_height_array[x_block, z_block] != 18 and prev_block_height_array[x_block, z_block] != 161 and prev_block_height_array[x_block, z_block] != 162 and prev_block_height_array[x_block, z_block] != 1985 and prev_block_height_array[x_block, z_block] != 99 and prev_block_height_array[x_block, z_block] != 100 and prev_block_height_array[x_block, z_block] != 398 and prev_block_height_array[x_block, z_block] != 38 and prev_block_height_array[x_block, z_block] != 39 and prev_block_height_array[x_block, z_block] != 40 and prev_block_height_array[x_block, z_block] != 1985: #checks from bottom up for a block until reaching highest block (not including air, water, or lava), priority being: z, x, y
+        if block == 0 and prev_block_height_array[x_block, z_block] != 0 and prev_block_height_array[x_block, z_block] != 18 and prev_block_height_array[x_block, z_block] != 161 and prev_block_height_array[x_block, z_block] != 162 and prev_block_height_array[x_block, z_block] != 32 and prev_block_height_array[x_block, z_block] != 99 and prev_block_height_array[x_block, z_block] != 100 and prev_block_height_array[x_block, z_block] != 37 and prev_block_height_array[x_block, z_block] != 38 and prev_block_height_array[x_block, z_block] != 39 and prev_block_height_array[x_block, z_block] != 40 and prev_block_height_array[x_block, z_block] != 175: #checks from bottom up for a block until reaching highest block (not including air, water, or lava), priority being: z, x, y
             height_array[x_block,z_block] = y_block + 60 #set the current value in height_array to the y coordinate
         prev_block_height_array[x_block,z_block] = block #set the current value in prev_block to the current block
         z_block += 1 #ensures loop can continue
         count += 1
-    min_heights = height_array.min(axis=1)
     return height_array
 
 def path_ew(min_heights, x, y, z):
@@ -81,7 +80,7 @@ def path_ns(min_heights, x, y, z):
             prev_item = item
         z = z + 1
 
-def path_se(mc, x, y, z):
+def path_se(mc, x, y, z): #test
     mc.setBlocks(x, y, z, x+14, y + 30, z+14, 0)
     radius = 9.5
     for angle in range(181, 270):
@@ -101,7 +100,7 @@ min_heights_ns = heights.min(axis=0)
 #path_ew(min_heights_ew, x, y, z)
 path_ns(min_heights_ns, x, y, z)
 #path_se(mc, x, y, z)
-
+print(heights)
 
 
 
