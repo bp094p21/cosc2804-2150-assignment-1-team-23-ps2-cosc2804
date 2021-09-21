d217099bcb961c8f35c2f9724db1d8d5ca986e2b
Jin <bp094p21@gmail.com>
2021-09-21 16:27:10 +1000

    template code for outdoor features

---
+++ village_generator/construction/building/property/components/veggie_patch.py
@@ -0,0 +1,25 @@
+from components.component import Component
+import block as b
+
+class VeggiePatch(Component):
+    # Class attributes
+    type: str = 'veggie_patch'
+    # Instance attributes
+    start_v3 = None
+    end_v3: int = None
+    block: b.Block = None
+    def __repr__(self):
+        return f"🖨  Printing object.__repr__:\n\n{type(self)}\start_v3: {self.start_v3},\nend_v3: {self.end_v3},\nblock: {self.block}\n"
+    def __init__(self, start_v3, end_v3, path_block):
+        self.start_v3 = start_v3
+        self.end_v3 = end_v3
+        self.block = path_block
+
+# TESTING
+if __name__ == '__main__':
+    from mcpi import minecraft as m
+    mc = m.Minecraft.create()
+    player_v3 = mc.player.getPos()
+    orientation = 0
+    entrance = VeggiePatch(start_V3=player_v3, end_v3=player_v3, door_block=b.COBBLESTONE)
+	
