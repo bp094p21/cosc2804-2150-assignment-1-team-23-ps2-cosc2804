1bef4648ece5e2bfb1af6087c50f75d3ef1098a6
Jin <bp094p21@gmail.com>
2021-09-22 17:59:40 +1000

    update object repr and refactor

--- village_generator/construction/building/property/property.py
+++ village_generator/construction/building/property/property.py
@@ -1,33 +1,34 @@
 from mcpi import vec3
 import theme as t
 import layout as l
-import components
 
 class Property:
     # __init__ inputs
-    location_v3: vec3.Vec3 = None 
-    orientation = None
-    theme: t.Theme = None       
-    entrance_edge: dict = None
-    house_type: str = None      
-    layout: l.Layout = None     
-    components: list = []
-    is_built = False
-
-    # Public functions
-    def __init__(self, location_v3: vec3.Vec3, orientation: int, theme, entrance_edge: dict, house_type: str, layout: l.Layout, components = []) -> None:
+    location_v3: vec3.Vec3  = None 
+    orientation: int        = None
+    theme: t.Theme          = None       
+    entrance_edge: dict     = None
+    house_type: str         = None      
+    layout: l.Layout        = None     
+    # Default attributes
+    components: list        = []
+    is_built: bool          = False
+    # Initializer
+    def __init__(
+        self,
+        location_v3: vec3.Vec3, 
+        orientation: int, 
+        theme: t.Theme, 
+        entrance_edge: dict, 
+        house_type: str, 
+        layout: l.Layout, 
+        ):
         self.location_v3 = location_v3
-        self.location_str = f"\nx: {location_v3.x}\ny: {location_v3.y}\nz: {location_v3.z}\n"
         self.orientation = orientation
         self.theme = theme
         self.entrance_edge = entrance_edge
         self.house_type = house_type
         self.layout = layout
-        self.components = components
-        return None
+        return
     def __repr__(self):
-        return f"🖨  Printing object.__repr__:\n\n{type(self)}\n\nlocation_v3: {self.location_v3},\norientation: {self.orientation},\ntheme: {self.theme.name},\nhouse_type: {self.house_type},\nis_built: {self.is_built}"
-    # Internal methods
-    def _instantiate_components(self):
-        # TODO: Use village_layout to randomly instantiate components to build
-        pass
+        return f"🖨  Printing {self.__class__.__name__} object details...\n\nlocation_v3: {self.location_v3},\norientation: {self.orientation},\ntheme: {self.theme.name},\nentrance_edge: {self.entrance_edge},\nhouse_type: {self.house_type},\nis_built: {self.is_built}.\n(layout and components not shown)\n"
