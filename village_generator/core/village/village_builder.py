9748839d244d5ed6c686b6ebf0728510377e05fa
Huntermuze <34412631+Huntermuze@users.noreply.github.com>
2021-09-22 11:15:43 +1000

    prepare generation test

--- village_generator/core/village/village_builder.py
+++ village_generator/core/village/village_builder.py
@@ -1,11 +1,14 @@
 import random
+from village_generator.core.terraform.terraformer import terraform_house_plot, terraform_road_plot
+
+from mcpi.minecraft import Minecraft
 import core.village_layout.predefined_layouts as pl
 
 from core.village.village_size import VillageSize
 from core.village_layout.layout import Layout
 from core.village_layout.plot import PlotType
 from core.terraform.terrain_scanner import scan_terrain, flatten_and_smooth
-from core.terraform.terraform import terraform
+from core.terraform.terraformer import terraform
 
 
 # TODO: rewrite this function so that the if-else statements for checking size are only written once.
@@ -17,7 +20,7 @@ def build_village(size, location, biome, mc):
 
     mc.postToChat('Generating village...')
     # TODO - finish.
-    flatten_and_smooth()
+    #flatten_and_smooth()
 
     _define_layout(size, biome, mc)
     selected_template = _select_random_template(size)
@@ -73,9 +76,9 @@ def _build_plots(fixed_ordinates, template, mc):
 
             if plot.plot_type == PlotType.BUILDING:
                 coordinates = fixed_ordinates[i][j]
-                _update_variable_house_height(coordinates, mc)
+                #_update_variable_house_height(coordinates, mc)
                 # terraform only for buildings & roads, to save resources and for it to look more natural with terrain.
-                terraform(mc, coordinates, coordinates[0] + 15, coordinates[2] + 15)
+                terraform_house_plot(mc, coordinates, coordinates[0] + 15, coordinates[2] + 15)
                 plot.item.set_location(coordinates)
                 plot.build_house()
             elif plot.plot_type == PlotType.ROAD:
@@ -83,39 +86,41 @@ def _build_plots(fixed_ordinates, template, mc):
                 if plot.entrance is True:
                     entrance_location = _transform_to_entrance_coords(*coordinates)
 
-                terraform(mc, coordinates, coordinates[0] + 15, coordinates[2] + 15)
+                terraform_road_plot(mc, coordinates, coordinates[0] + 15, coordinates[2] + 15)
                 plot.build_road(mc, fixed_ordinates[i][j])
 
     return entrance_location
 
 
-# TODO: finish and test.
-def _update_variable_house_height(house_location, mc):
-    highest_peak = mc.getHeight(house_location[0], house_location[2])
+# # TODO: finish and test.
+# def _update_variable_house_height(house_location, mc):
+#     highest_peak = mc.getHeight(house_location[0], house_location[2])
 
-    # if the peak is really high up (5 blocks+) then flatten it a bit.
-    # or maybe just let it be high up, and have the roads steeper.
-    # or better yet, with the terraformer, make it clear the land so that the increments of height are shallow and not
-    # super steep. So only increments by 1 at a time, with minimum distance of 2 blocks from the previous level
-    # to the next level. That way it is guaranteed to fit flush. This is a secondary measure.
+#     # if the peak is really high up (5 blocks+) then flatten it a bit.
+#     # or maybe just let it be high up, and have the roads steeper.
+#     # or better yet, with the terraformer, make it clear the land so that the increments of height are shallow and not
+#     # super steep. So only increments by 1 at a time, with minimum distance of 2 blocks from the previous level
+#     # to the next level. That way it is guaranteed to fit flush. This is a secondary measure.
 
-    if highest_peak - house_location[1] > 4:
-        # implement some sort of land smoother/flattener/brush tool that will smooth out the land and make it more real.
-        pass
+#     if highest_peak - house_location[1] > 4:
+#         # implement some sort of land smoother/flattener/brush tool that will smooth out the land and make it more real.
+#         pass
 
-    house_location[1] = house_location[1] + highest_peak
+#     house_location[1] = house_location[1] + highest_peak
 
 
 def _transform_to_entrance_coords(x, y, z):
     return x + 7.5, y, z + 7.5
 
 
-def _generate_fixed_ordinates(max_z: int, max_x: int, x_coord: int, y_coord: int, z_coord: int) -> list:
+def _generate_fixed_ordinates(max_z: int, max_x: int, x_coord: int, z_coord: int, mc: Minecraft) -> list:
     temp = []
     for z in range(0, max_z + 1):
         temp.append([])
         for x in range(0, max_x + 1):
-            temp[z].append([x_coord + x * 15, y_coord, z_coord + z * 15])
+            temp_x = x_coord + x * 15
+            temp_z = z_coord + z * 15
+            temp[z].append([temp_x, mc.getHeight(temp_x, temp_z), temp_z])
 
     return temp
 
