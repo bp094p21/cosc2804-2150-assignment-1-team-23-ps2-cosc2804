45aca569ab3388f15f6356bf4d7d0061b846268e
Jin <bp094p21@gmail.com>
2021-09-24 17:47:46 +1000

    add bed building func

--- village_generator/construction/building/property/tradies/decorator.py
+++ village_generator/construction/building/property/tradies/decorator.py
@@ -5,3 +5,8 @@ else:
 
 class Decorator(Tradie):
     trade = 'decorating'
+    def build_component(self, component, mc):
+        if component.type == 'bed':
+            self._build_bed(component, mc)
+    def _build_bed(self, bed, mc):
+        self._one_block(bed.v3, bed.block, mc)
