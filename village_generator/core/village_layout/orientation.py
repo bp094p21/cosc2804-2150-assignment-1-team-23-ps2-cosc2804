5edb32ed1c8c1f219ea90968d0e1906e4d69d55c
Huntermuze <34412631+Huntermuze@users.noreply.github.com>
2021-09-26 00:36:22 +1000

    add comments

--- village_generator/core/village_layout/orientation.py
+++ village_generator/core/village_layout/orientation.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-
+"""The orientation of each plot's entrance. Depending on the selected orientation, the houses will be facing this direction."""
 class Orientation(Enum):
     WEST = 0
     NORTH = 1
