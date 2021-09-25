a2578f17d8b1d5272118b7eab7a92279fcf26198
Huntermuze <34412631+Huntermuze@users.noreply.github.com>
2021-09-26 00:37:04 +1000

    tidy up structure classes

--- village_generator/construction/structure.py
+++ village_generator/construction/structure.py
@@ -1,21 +1,16 @@
-# A collection of two data classes to allow interoperability with the property and misc package's system.
+"""A collection of two data classes to allow interoperability with the property and misc package's system."""
 
+# Represents an individual house.
 class House:
-    mc_instance = None
-    
     def __init__(self, mc_instance, orientation, theme):
-        if mc_instance is None:
-            House.mc_instance = mc_instance
-
+        # A static/class variable for mc_instance would be much more suitable here, as there is only ever going to be one shared instance of the Minecraft class. However,
+        # due to time shortage, this has been left out/
+        self.mc_instance = mc_instance
         self.orientation = orientation
         self.theme = theme
 
-
+# Represents an individual misc building.
 class Misc:
-    mc_instance = None
-
     def __init__(self, mc_instance, biome):
-        if mc_instance is None:
-            Misc.mc_instance = mc_instance
-
+        self.mc_instance = mc_instance
         self.biome = biome
