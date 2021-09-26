09954b4fd7ad761ac178924b1dc4002746548d2e
huntermuze <mbyamen@gmail.com>
2021-09-26 17:41:35 +1000

    tidy documentation

--- village_generator/main.py
+++ village_generator/main.py
@@ -3,7 +3,7 @@ from core.village.village_builder import build_village
 from core.village.village_size import VillageSize
 import random
 
-# The main class file where all the action happens (not really - just one line)!
+# The main class file where all the action happens (not really - just a few lines)!
 
 
 if __name__ == '__main__':
@@ -15,12 +15,12 @@ if __name__ == '__main__':
     # Upon invocation will generate a village where the player is standing.
     rand_num = random.randint(0, 2) #0 is small, 1 is medium, 2 is large
     if rand_num == 0:
-        mc.postToChat('This village being generated to you is of small size. Please wait..')
+        mc.postToChat('Sit tight whilst we generate a small sized village for you!')
         build_village(VillageSize.SMALL, (x + 1, y, z + 1), mc.player.getBiome(), mc)
     elif rand_num == 1:
-        mc.postToChat('This village being generated to you is of medium size. Please wait..')
+        mc.postToChat('Sit tight whilst we generate a medium sized village for you!')
         build_village(VillageSize.MEDIUM, (x + 1, y, z + 1), mc.player.getBiome(), mc)
     else:
-        mc.postToChat('This village being generated to you is of large size. Please wait..')
+        mc.postToChat('Sit tight whilst we generate a large sized village for you!')
         build_village(VillageSize.LARGE, (x + 1, y, z + 1), mc.player.getBiome(), mc)
 
