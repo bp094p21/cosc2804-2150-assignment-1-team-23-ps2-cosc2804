1b028ea134e568008d9620fb94d46ca1eaf2e8e0
Jin Heock <fttx5525@gmail.com>
2021-09-26 12:51:24 +1000

    modify boundary SPECS

--- village_generator/construction/building/property/component/boundary.py
+++ village_generator/construction/building/property/component/boundary.py
@@ -11,8 +11,10 @@ class Boundary(Component):
     block: Block = None
 
     ## Height and stepping frequency dependent on Entrance
-    def __init__(self, block=STONE):
+    def __init__(self, start_v3, end_v3, block=STONE):
         self.block = block
+        self.start_v3 = start_v3
+        self.end_v3 = end_v3
 
     def __repr__(self):
         return f"🖨  Printing object.__repr__:\n\n{type(self)}\n\length: {self.length},\nblock: {self.block}\n"
