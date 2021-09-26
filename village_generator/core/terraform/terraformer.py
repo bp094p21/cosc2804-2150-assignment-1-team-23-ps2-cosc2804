87429c11c9e11ef55f00e77c3f561e5c59c675c3
Lachlan <s3896004@student.rmit.edu.au>
2021-09-26 14:17:56 +1000

    Account for different village sizes (can now generate any size + border around them)

--- village_generator/core/terraform/terraformer.py
+++ village_generator/core/terraform/terraformer.py
@@ -14,6 +14,16 @@ def terraform_entire_land(mc, biome, x1, y1, z1, x2, y2, z2):
     ground_fill_block = iterate_ground_filler(GROUND_FILLER, biome)
     mc.setBlocks(x1, y1 - 1, z1, x2, y1 - 1, z2, ground_fill_block)
 
+    #Then build a border around the village to clear any liquid seeping in
+    # Create a border around the perimeter of the village (1 area difference)
+    x, y, z = mc.player.getTilePos()
+    block = mc.getBlock(x, y-1, z)
+    mc.postToChat(block)
+    mc.setBlocks(x1, 40, z1 - 1, x2, y, z1 - 1, block) 
+    mc.setBlocks(x1 - 1, 40, z1, x1 - 1, y, z2, block)
+    mc.setBlocks(x1, 40, z2 + 1, x2, y, z2 + 1, block)
+    mc.setBlocks(x2 + 1, 40, z1, x2 + 1, y, z2, block)
+
 
 # Searches the dictionary to see if the biome string is in any of the keys, and if it is, return the block id to pave
 # the floor.
@@ -48,34 +58,34 @@ def iterate_ground_filler(keys, target):
 # # GRASS_TALL = 31
 
 # individual terraforming functions to accommodate for both shared and individual functionality.
-# def terraform_house_plot(mc, start_loc, max_x, max_z):
+# def terraform_house_plot(mc, start_loc, x2, z2):
 #     print('LOG >> TERRAFORMING HOUSE PLOT')
-#     _terraform(mc, start_loc, max_x, max_z)
+#     _terraform(mc, start_loc, x2, z2)
 
 
-# def terraform_road_plot(mc, start_loc, max_x, max_z):
+# def terraform_road_plot(mc, start_loc, x2, z2):
 #     print('LOG >> TERRAFORMING ROAD PLOT')
-#     _terraform(mc, start_loc, max_x, max_z)
+#     _terraform(mc, start_loc, x2, z2)
 
 
 # # base terraforming function that handles the shared functionality.
-# def _terraform(mc, start_loc, max_x, max_z):
+# def _terraform(mc, start_loc, x2, z2):
 #     x, y, z = start_loc
 #     print('LOG >> GETTING HIGHEST/LOWEST Y')
 #     print('X1=' + str(x))
 #     print('Y1=' + str(y))
 #     print('Z1=' + str(z))
-#     print('MAX_X=' + str(max_x))
-#     print('MAX_Z=' + str(max_z))
-#     max_y, min_y = mc.getHighestAndLowestYInRegion(x, z, max_x, max_z)
+#     print('x2=' + str(x2))
+#     print('z2=' + str(z2))
+#     y2, y1 = mc.getHighestAndLowestYInRegion(x, z, x2, z2)
 #     print('LOG >> RECEIVED HIGHEST/LOWEST Y')
-#     start_loc = (x, min_y, z)
-#     _clear_disallowed_blocks(mc, start_loc, max_x, max_y, max_z)
+#     start_loc = (x, y1, z)
+#     _clear_disallowed_blocks(mc, start_loc, x2, y2, z2)
 
-# def _clear_disallowed_blocks(mc, start_loc, max_x, max_y, max_z):
+# def _clear_disallowed_blocks(mc, start_loc, x2, y2, z2):
 #     print('LOG >> CLEARING DISALLOWED BLOCKS')
 #     x, y, z = start_loc
 #     # Implemented on back-end because the transfer between sockets was a big bottleneck and
 #     # slowed down program tremendously.
-#     mc.removeBlocksInRegion(x, y, z, max_x, max_y, max_z, *DISALLOWED_BLOCKS)
+#     mc.removeBlocksInRegion(x, y, z, x2, y2, z2, *DISALLOWED_BLOCKS)
 #     print('LOG >> CLEARED DISALLOWED BLOCKS')
--- village_generator/main.py
+++ village_generator/main.py
@@ -1,6 +1,7 @@
 from mcpi.minecraft import Minecraft
 from core.village.village_builder import build_village
 from core.village.village_size import VillageSize
+import random
 
 # The main class file where all the action happens (not really - just one line)!
 
@@ -12,13 +13,14 @@ if __name__ == '__main__':
     block = mc.getBlock(x, y-1, z)
 
     # Upon invocation will generate a village where the player is standing.
-    build_village(VillageSize.LARGE, (x + 1, y, z + 1), mc.player.getBiome(), mc)
-    # Create a border
-    village_len_large = (15 * 7) + 1
-    village_wid_large = (15 * 6) + 1
-    comfortable_ground = 40 #Base level for border in order to always be efficient
-    mc.setBlocks(x, comfortable_ground, z, x + village_wid_large, y, z, block) 
-    mc.setBlocks(x, comfortable_ground, z, x, y, z + village_len_large, block)
-    mc.setBlocks(x, comfortable_ground, z + village_len_large, x + village_wid_large, y, z + village_len_large, block)
-    mc.setBlocks(x + village_wid_large, comfortable_ground, z, x + village_wid_large, y, z + village_len_large, block)
+    rand_num = random.randint(0, 2) #0 is small, 1 is medium, 2 is large
+    if rand_num == 0:
+        mc.postToChat('This village being generated to you is of small size. Please wait..')
+        build_village(VillageSize.SMALL, (x + 1, y, z + 1), mc.player.getBiome(), mc)
+    elif rand_num == 1:
+        mc.postToChat('This village being generated to you is of medium size. Please wait..')
+        build_village(VillageSize.MEDIUM, (x + 1, y, z + 1), mc.player.getBiome(), mc)
+    else:
+        mc.postToChat('This village being generated to you is of large size. Please wait..')
+        build_village(VillageSize.LARGE, (x + 1, y, z + 1), mc.player.getBiome(), mc)
 
